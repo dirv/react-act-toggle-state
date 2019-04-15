4b0145ad79271cd9fcb575f44421283e62e3b806
Sunil Pai <threepointone@fb.com>
2019-04-15 13:08:37 +0100

    how to test sync + async

--- App.test.js
+++ App.test.js
@@ -8,8 +8,8 @@ describe('App', () => {
 
   beforeEach(() => {
     container = document.createElement('div');
-    fetcher = jest.fn(() => Promise.resolve(() => ({ })));
-    ReactDOM.render(<App fetcher={fetcher}/>, container);
+    fetcher = jest.fn(() => Promise.resolve(() => ({})));
+    ReactDOM.render(<App fetcher={fetcher} />, container);
   });
 
   const form = () => container.querySelector('form');
@@ -17,21 +17,20 @@ describe('App', () => {
   const element = name => container.querySelector(name);
 
   describe('submitting indicator', () => {
-    it('displays indicator when form is submitting', async () => {
-      ReactTestUtils.Simulate.submit(form());
-      await act(async () => {
-        expect(element('.submittingIndicator')).not.toBeNull();
-      });
-    });
-
-    it('initially does not display the submitting indicator', () => {
-      expect(element('.submittingIndicator')).toBeNull();
-    });
-
-    it('hides indicator when form has submitted', async () => {
-      await act(async () => {
+    it('displays indicator when form is submitting, and hides after', async () => {
+      // your situation is a bit granular since you're testing
+      // stuff happening in rapid succession.
+      // so let's start with a sync act() to test the initial ui
+      act(() => {
         ReactTestUtils.Simulate.submit(form());
       });
+      expect(element('.submittingIndicator')).not.toBeNull();
+      // now wait for a 'tick' and for ui to stabilize
+      await act(async () => {
+        // this might seem odd, but it matches user behaviour, to 'wait' a smidge.
+        // similar tests could also, for eg, resolve the fetcher() mock inside this scope manually.
+      });
+
       expect(element('.submittingIndicator')).toBeNull();
     });
   });
